48c2813d7fa13c3e2c5cb7fb7611913b706e9310
yerriswamykonanki <yerriswamykonanki@gmail.com>
2017-12-28 15:20:27 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,6 +1,9 @@
+
+def call() {
+ def notifySuccessful(){
 emailext (
  attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''<span style=\'line-height: 22px; font-family: Candara; padding: 10.5px; font-size: 15px; word-break: break-all; word-wrap: break-word; \'>
  <tr><td><a href="$BUILD_URL">Click Here</a> to view build result</h2><br><h3>Please find below, the build logs and other files.</td></tr></table>
- </span>''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com, sunil.boga@ggktech.com'
+ </span>''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com'
  )
-}
+ }}
