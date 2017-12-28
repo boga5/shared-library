86abe8f54312ad982653405c76e9b756444a3667
yerriswamykonanki <yerriswamykonanki@gmail.com>
2017-12-28 14:53:02 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,17 @@
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  
+   stage ('Tests') {
+                    parallel 'static': {
+                        sh "echo 'shell scripts to run static tests...'"
+                    },
+                    'unit': {
+                        sh "echo 'shell scripts to run unit tests...'"
+                    },
+                    'integration': {
+                        sh "echo 'shell scripts to run integration tests...'"
+                    }
+                }
+  
+  
 }
