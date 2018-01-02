163ee2640766f60cd4ce9da32fbf290148c08171
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-02 10:18:37 +0530

    Create Reading_varibles.groovy

---
+++ vars/Reading_varibles.groovy
@@ -0,0 +1,13 @@
+ JobName = "${JOB_NAME}"
+           lock_resource_name = "jenkins-file"
+            Sonar_project_name = "jenkins-file"
+		/*	if("${BRANCH_NAME}".startsWith('PR-'))
+			{
+             	lock_resource_name = JobName.substring(0 , JobName.indexOf("/"))+"_"+"${CHANGE_TARGET}"
+                Sonar_project_name = lock_resource_name + "PR"
+			}
+			else
+			{
+				 lock_resource_name = JobName.substring(0 , JobName.indexOf("/"))+"_"+"${BRANCH_NAME}"
+				 Sonar_project_name = lock_resource_name
+			} */
