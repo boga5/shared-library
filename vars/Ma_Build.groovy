6faa643ccfe2290faca0a43adf18c4f44c560af9
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-02 12:01:20 +0530

    Update Ma_Build.groovy

--- vars/Ma_Build.groovy
+++ vars/Ma_Build.groovy
@@ -4,11 +4,11 @@ Reason = "Maven Build Failed"
    rtMaven.deployer.deployArtifacts = false  //this will not publish artifacts soon after build succeeds //
    rtMaven.tool = 'maven'       //Defining maven tool //
    // Maven build starts here //
-   withSonarQubeEnv {
+   //withSonarQubeEnv {
     def mvn_version = tool 'maven'
     withEnv( ["PATH+MAVEN=${mvn_version}/bin",'Sonar_Project_Name=' + "${temp.Sonar_project_name}"]  ) {
      buildInfo = rtMaven.run pom: 'pom.xml', goals: 'clean install -Dmaven.test.skip=true $SONAR_MAVEN_GOAL -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.projectKey=${Sonar_Project_Name} -Dsonar.projectName=${Sonar_Project_Name}'
      
     }
-   }
+   //}
 }
