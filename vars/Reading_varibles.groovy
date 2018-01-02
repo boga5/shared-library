d0e7140ef71e2692cc12f49ae5425da600e2c044
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-02 10:38:53 +0530

    Update Reading_varibles.groovy

--- vars/Reading_varibles.groovy
+++ vars/Reading_varibles.groovy
@@ -1,7 +1,10 @@
+
 def call() {
 JobName = "${JOB_NAME}"
            lock_resource_name = "jenkins-file"
            Sonar_project_name = "jenkins-file"
+	println lock_resource_name
+           println Sonar_project_name
 	/*	if("${BRANCH_NAME}".startsWith('PR-'))
 			{
              	lock_resource_name = JobName.substring(0 , JobName.indexOf("/"))+"_"+"${CHANGE_TARGET}"
