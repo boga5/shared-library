01694c6a81ab1d37d095a290ef97c04a65d00c2d
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-02 11:26:50 +0530

    Update Reading_varibles.groovy

--- vars/Reading_varibles.groovy
+++ vars/Reading_varibles.groovy
@@ -1,10 +1,17 @@
 
+class A {
+	def lock_resource_name 
+         def Sonar_project_name 
+}
 def call() {
 JobName = "${JOB_NAME}"
-           lock_resource_name = "jenkins-file"
-           Sonar_project_name = "jenkins-file"
-	println lock_resource_name
-           println Sonar_project_name
+	A a = new A()
+	
+           a.lock_resource_name = "jenkins-file"
+           a.Sonar_project_name = "jenkins-file"
+	println a.lock_resource_name
+           println a.Sonar_project_name
+	return a
 	/*	if("${BRANCH_NAME}".startsWith('PR-'))
 			{
              	lock_resource_name = JobName.substring(0 , JobName.indexOf("/"))+"_"+"${CHANGE_TARGET}"
