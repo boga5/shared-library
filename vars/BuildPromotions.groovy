a1558736c31fffa5358c220f96ce5264c65c663e
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 15:09:50 +0530

    Update BuildPromotions.groovy

--- vars/BuildPromotions.groovy
+++ vars/BuildPromotions.groovy
@@ -1,21 +1,22 @@
 def call(def buildInfo, def release_repo, def snapshot_repo, def server)
 {
-Reason = "Build Promotions Failed"
-			def promotionConfig = [
-				// Mandatory parameters
-				'buildName'          : buildInfo.name,
-				'buildNumber'        : buildInfo.number,
-				'targetRepo'         : release_repo,
+	def Reason = "Build Promotions Failed"
+	def promotionConfig = [
+	// Mandatory parameters
+	'buildName'          : buildInfo.name,
+	'buildNumber'        : buildInfo.number,
+	'targetRepo'         : release_repo,
+		
+	// Optional parameters
+	'comment'            : 'PROMOTION SUCCESSFULLY COMPLETED',
+	'sourceRepo'         : snapshot_repo,
+	'status'             : 'Released',
+	'includeDependencies': false,
+	'copy'               : false,
+	'failFast'           : true
+	]
 	 
-				// Optional parameters
-				'comment'            : 'PROMOTION SUCCESSFULLY COMPLETED',
-				'sourceRepo'         : snapshot_repo,
-				'status'             : 'Released',
-				'includeDependencies': false,
-				'copy'               : false,
-				'failFast'           : true
-			]
-	 
-			// Interactive promotion of Builds in Artifactory server from Jenkins UI //
-			Artifactory.addInteractivePromotion server: server, promotionConfig: promotionConfig, displayName: "Promotions Time" //this need human interaction to promote
+	// Interactive promotion of Builds in Artifactory server from Jenkins UI //
+	Artifactory.addInteractivePromotion server: server, promotionConfig: promotionConfig, displayName: "Promotions Time" //this need human interaction to promote
+	return Reason 
 }
