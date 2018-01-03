736170c689c2675e18b12e9c4ea1707bdb93aef1
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 15:02:43 +0530

    Update PushingArtifacts.groovy

--- vars/PushingArtifacts.groovy
+++ vars/PushingArtifacts.groovy
@@ -2,5 +2,6 @@ def call(def rtMaven, def buildInfo, def server) {
  Reason = "Artifacts Deployment Failed"
  rtMaven.deployer.deployArtifacts buildInfo
  server.publishBuildInfo buildInfo
+ return Reason
 }
 //
