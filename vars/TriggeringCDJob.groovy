70a3db13d0d2ba75013a0bf16ca092799a9c8d62
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 11:03:17 +0530

    Update TriggeringCDJob.groovy

--- vars/TriggeringCDJob.groovy
+++ vars/TriggeringCDJob.groovy
@@ -1,5 +1,5 @@
 def call(def Sonar_project_name)
 {
   CD_Job_name = Sonar_project_name + "_QA"
-  build job: "Docker_registry"//, parameters: [[$class: 'StringParameterValue', name: 'var1', value: 'var1_value']]
+  build job: 'Docker_registry'//, parameters: [[$class: 'StringParameterValue', name: 'var1', value: 'var1_value']]
 }
