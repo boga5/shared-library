11c1163930693355d6b9ce6324f5963acf08e4b3
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 15:05:07 +0530

    Update SQReportsCreation.groovy

--- vars/SQReportsCreation.groovy
+++ vars/SQReportsCreation.groovy
@@ -1,4 +1,5 @@
 def call(def Sonar_project_name) {
-  Reason = "Reports creation Failed"
+  def Reason = "Reports creation Failed"
 	sh """ curl "http://10.240.17.12:9000/sonar/api/resources?resource=${Sonar_project_name}&metrics=bugs,vulnerabilities,code_smells,duplicated_blocks" > output.json """
+  return Reason
 }
