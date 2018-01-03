e2b174a7d9f97643505424be62c44827aa18274e
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 11:29:12 +0530

    Create BuildPromotions.groovy

---
+++ vars/BuildPromotions.groovy
@@ -0,0 +1,21 @@
+def call(def buildInfo, def release_repo, def snapshot_repo, def server)
+{
+Reason = "Build Promotions Failed"
+			def promotionConfig = [
+				// Mandatory parameters
+				'buildName'          : buildInfo.name,
+				'buildNumber'        : buildInfo.number,
+				'targetRepo'         : release_repo,
+	 
+				// Optional parameters
+				'comment'            : 'PROMOTION SUCCESSFULLY COMPLETED',
+				'sourceRepo'         : snapshot_repo,
+				'status'             : 'Released',
+				'includeDependencies': false,
+				'copy'               : false,
+				'failFast'           : true
+			]
+	 
+			// Interactive promotion of Builds in Artifactory server from Jenkins UI //
+			Artifactory.addInteractivePromotion server: server, promotionConfig: promotionConfig, displayName: "Promotions Time" //this need human interaction to promote
+}
