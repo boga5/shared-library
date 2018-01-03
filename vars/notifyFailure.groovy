7a843f18a4df591e2ea8dbd256accf0a7223088a
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-01-03 16:05:40 +0530

    Update notifyFailure.groovy

--- vars/notifyFailure.groovy
+++ vars/notifyFailure.groovy
@@ -1,3 +1,3 @@
 def call(def Reason){
-  emailext (attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''${SCRIPT, template="email_template_failure.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com')
+  emailext (attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''${SCRIPT, template="email_template_failure.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com, sneha.kailasa@ggktech.com, sunil.boga@ggktech.com')
 }
