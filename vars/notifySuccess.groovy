539fa1759452e18ef21a7cbbfc4f882fc065f147
Sunil Boga <30490515+boga5@users.noreply.github.com>
2018-01-03 17:11:48 +0530

    Update notifySuccess.groovy

--- vars/notifySuccess.groovy
+++ vars/notifySuccess.groovy
@@ -1,5 +1,6 @@
-def call(def Reason) {
-emailext (
+def call(def Reason, def WORKSPACE) {
+properties([[$class: 'EnvInjectJobProperty', info: [loadFilesFromMaster: false, propertiesContent: "JobWorkSpace=${WORKSPACE}"], keepBuildVariables: true, keepJenkinsSystemVariables: true, on: true]])
+ emailext (
  attachLog: true, attachmentsPattern: '*.html, output.xml', body: '''
  ${SCRIPT, template="email_template_success.groovy"}''', subject: '$DEFAULT_SUBJECT', to: 'yerriswamy.konanki@ggktech.com, sneha.kailasa@ggktech.com, sunil.boga@ggktech.com'
  )
